51db6b283b28e70b3556a39cddffafe06d0e71d9
Velluvia <hello@velluvia.co.uk>
2026-08-02 19:51:48 +0100

    Add files via upload

--- components/HeroLogo.tsx
+++ components/HeroLogo.tsx
@@ -2,7 +2,7 @@ export default function HeroLogo() {
   return (
     <div className="hero-logo-figure" aria-hidden="true">
       <div className="hero-logo-frame">
-        <img src="/images/hero-logo.png" alt="" className="hero-logo-img" />
+        <img src="/images/hero-logo-transparent.png" alt="Velluvia" className="hero-logo-img" />
         <div className="hero-logo-shine" />
       </div>
     </div>
